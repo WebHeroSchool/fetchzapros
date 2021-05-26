f4245c733148d2481bab0ca7e0dd257a207b7123
Кириллова Наталья <natalka.kirillov@mail.ru>
2021-05-26 22:22:24 +0300

    запрос1

---
+++ scripts.js
@@ -0,0 +1,21 @@
+fetch('https://api.github.com/users/foxx4')
+.then(res => res.json())
+.then(json => console.log(json.avatar_url))
+.catch(err => console.log(err));
+
+fetch('https://api.github.com/users/foxx4')
+.then(res => res.json())
+.then(json => console.log(json.bio))
+.catch(err => console.log(err));
+
+fetch('https://api.github.com/users/foxx4')
+.then(res => res.json())
+.then(json => console.log(json.html_url))
+.catch(err => console.log(err));
+
+fetch('https://api.github.com/?username=foxx4')
+.then(res => res.json())
+.then(json => console.log(json.name))
+.catch(err => console.log(err));
+
+
